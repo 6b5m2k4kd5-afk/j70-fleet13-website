a260b17cbc4da8b7bde7ebaa06110987affc0109
6b5m2k4kd5-afk <6b5m2k4kd5@privaterelay.appleid.com>
2026-05-18 12:40:30 -0500

    Fix: Update .eleventy.js configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
-import { readFileSync } from 'fs';
-import yaml from 'js-yaml';
-import Image from '@11ty/eleventy-img';
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const markdownIt = require('markdown-it');
 
-export default function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
 
   // Watch CSS and config files for changes
   eleventyConfig.addWatchTarget('./src/**/*.css');
@@ -12,14 +13,9 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css');
   eleventyConfig.addPassthroughCopy('./fleets/**/images');
   eleventyConfig.addPassthroughCopy('./fleets/**/logo.*');
-  eleventyConfig.addPassthroughCopy('./.htaccess');
 
   // Global data - load fleet configs
   eleventyConfig.addGlobalData('fleets', function() {
-    const fs = require('fs');
-    const path = require('path');
-    const yaml = require('js-yaml');
-
     const fleetsDir = 'fleets';
     const fleets = {};
 
@@ -29,39 +25,18 @@ export default function(eleventyConfig) {
       fleetDirs.forEach(fleetDir => {
         const configPath = path.join(fleetsDir, fleetDir, 'config.yml');
         if (fs.existsSync(configPath)) {
-          const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
-          fleets[fleetDir] = config;
-          fleets[fleetDir].slug = fleetDir;
+          try {
+            const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
+            fleets[fleetDir] = config;
+            fleets[fleetDir].slug = fleetDir;
+          } catch(e) {
+            console.warn(`Error loading config for ${fleetDir}:`, e.message);
+          }
         }
       });
     }
 
     return fleets;
-  });
-
-  // Image optimization filter
-  eleventyConfig.addNunjucksAsyncFilter('image', async function(src, alt) {
-    try {
-      if (!src) return '';
-
-      let metadata = await Image(src, {
-        widths: [300, 600, 1200],
-        formats: ['webp', 'jpeg'],
-        outputDir: './_site/img/'
-      });
-
-      let imageAttributes = {
-        alt: alt || '',
-        sizes: '(min-width: 1024px) 1024px, 100vw',
-        loading: 'lazy',
-        decoding: 'async'
-      };
-
-      return Image.generateHTML(metadata, imageAttributes);
-    } catch(e) {
-      console.warn(`Image optimization failed for ${src}:`, e.message);
-      return `<img src="${src}" alt="${alt || ''}" />`;
-    }
   });
 
   // Date formatting filter
@@ -79,20 +54,7 @@ export default function(eleventyConfig) {
     return date.toISOString().split('T')[0];
   });
 
-  // Get fleet config by slug
-  eleventyConfig.addFilter('getFleetConfig', function(fleetSlug) {
-    const fleets = this.ctx.globals.fleets;
-    return fleets[fleetSlug] || {};
-  });
-
-  // Get all fleet slugs
-  eleventyConfig.addFilter('getAllFleetSlugs', function() {
-    const fleets = this.ctx.globals.fleets;
-    return Object.keys(fleets);
-  });
-
   // Markdown library with plugins
-  let markdownIt = require('markdown-it');
   let md = new markdownIt({
     html: true,
     breaks: true,
@@ -109,9 +71,8 @@ export default function(eleventyConfig) {
       includes: '_includes',
       layouts: '_includes/layouts'
     },
-    templateFormats: ['md', 'njk', 'html', 'yml'],
+    templateFormats: ['md', 'njk', 'html'],
     markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk'
+    htmlTemplateEngine: 'njk'
   };
-}
+};
